9d34941ec467e0bc103329f0bffc1014a5a02464
Hashed <z-donnelly@hvcc.edu>
2020-01-25 18:10:26 -0500

    add instagram access to home page, displaying ig photos

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
 import ReadLink from '../components/read-link';
 import Hero from '../components/hero';
+import Insta from '../components/insta';
 
 export default () => {
   const posts = usePosts();
@@ -19,6 +20,7 @@ export default () => {
         {posts.map(post => (
           <PostPreview key={post.slug} post={post} />
         ))}
+        <Insta />
       </Layout>
     </>
   );
